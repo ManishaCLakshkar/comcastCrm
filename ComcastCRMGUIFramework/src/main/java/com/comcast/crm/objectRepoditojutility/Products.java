e5ad26eb9419f236b5bae3bb3fbe850715c95383
Manisha <manisha95130@gmail.com>
2024-05-13 05:09:24 -0700

    commit product class new ele added

---
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectRepoditojutility/Products.java
@@ -0,0 +1,13 @@
+package com.comcast.crm.objectRepoditojutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+	@FindBy(xpath="//img[@alt='Create Product...']")
+	private WebDriver createProdutImgBtn;
+	
+	@FindBy(name="search")
+	private WebDriver ele2;
+
+}
